b2a016ccbdbb70ebb933f5d44a6bc60d5c73ef63
John Meguerian <johnmeguerian@gmail.com>
2023-02-14 13:46:50 -0500

    blank out repo

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./layouts/**/*.html"],
-	darkMode: "media",
-	theme: {
-		extend: {}
-	},
-	plugins: [require('@tailwindcss/typography')]
-}
