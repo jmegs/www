f833ec39058f5fa6abf50d6c3852b710727e8938
John Meguerian <johnmeguerian@gmail.com>
2023-09-19 17:23:28 -0400

    wip: add clock to nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,24 @@
+'use client';
+
 import NavItem from "@/components/navitem";
 import Link from 'next/link'
+import { useEffect, useState } from "react";
+
+
 
 export default function Navbar() {
   const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || 'DEVMODE'
+  const [time, setTime] = useState(() => getFormattedTime())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = getFormattedTime() 
+      setTime(now)
+    }, 1000 * 10)
+
+    return () => clearInterval(interval)
+  })
+
   return (
     <div className="flex justify-between px-8 py-4">
       <div className="flex gap-x-4">
@@ -13,8 +29,14 @@ export default function Navbar() {
       </div>
       <div className="flex gap-x-4">
         <NavItem label={sha.slice(0, 7)} icon="commit" isActive={false} />
-        <NavItem label="Index" icon="location" isActive={false} />
+        <NavItem label={`NYC ${time}`} icon="location" isActive={false} />
       </div>
     </div>
   )
+}
+
+function getFormattedTime() {
+  let now = Date.now()
+  let formatted = Intl.DateTimeFormat("en-US", { hour: "2-digit", minute: "2-digit", timeZone: "America/New_York" }).format(now).slice(0, -3)
+  return formatted
 }
