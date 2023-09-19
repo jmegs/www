f9de1fb322559be190481cb416df777c6d8b7692
John Meguerian <johnmeguerian@gmail.com>
2023-09-19 12:04:53 -0400

    wip: add blob

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
+import './blob.css'
 import type { Metadata } from 'next'
+import Blob from '@/components/blob'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -9,7 +11,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Blob />
+      </body>
     </html>
   )
 }
