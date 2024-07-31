8b331f75b11a37f56147fdefa0a2ddf185ce65fc
John Meguerian <johnmeguerian@gmail.com>
2024-07-31 14:35:37 -0400

    wip: og image url fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,38 +5,41 @@ import icon from "astro-icon"
 import { getLatestCommitHash } from "./src/utils/git"
 
 const fontaineOptions = {
-  fallbacks: ["system-ui", "sans-serif"],
-  resolvePath: (id) => `file:///public/fonts/${id}`,
+	fallbacks: ["system-ui", "sans-serif"],
+	resolvePath: (id) => `file:///public/fonts/${id}`,
 }
 
 function gitHashIntegration() {
-  return {
-    name: "git-hash-integration",
-    hooks: {
-      "astro:config:setup": ({ updateConfig }) => {
-        const commitHash = getLatestCommitHash()
-        updateConfig({
-          vite: {
-            define: {
-              "import.meta.env.COMMIT_HASH": JSON.stringify(commitHash),
-            },
-          },
-        })
-      },
-    },
-  }
+	return {
+		name: "git-hash-integration",
+		hooks: {
+			"astro:config:setup": ({ updateConfig }) => {
+				const commitHash = getLatestCommitHash()
+				updateConfig({
+					vite: {
+						define: {
+							"import.meta.env.COMMIT_HASH": JSON.stringify(commitHash),
+						},
+					},
+				})
+			},
+		},
+	}
 }
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [FontaineTransform.vite(fontaineOptions)],
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-    gitHashIntegration(),
-  ],
+	site: process.env.VERCEL_URL
+		? `https://${process.env.VERCEL_URL}`
+		: "https://johnmeguerian.com",
+	vite: {
+		plugins: [FontaineTransform.vite(fontaineOptions)],
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		icon(),
+		gitHashIntegration(),
+	],
 })
