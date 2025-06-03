d29a57e4416f7a811a621833be1dac5bfcebe43b
John Meguerian <johnmeguerian@gmail.com>
2025-06-03 14:49:12 -0400

    Nextjs -> Astro
    we don't need react for this literal single page of nothing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		responsiveImages: true,
+		fonts: [
+			{
+				provider: "local",
+				name: "Mondwest",
+				cssVariable: "--font-serif",
+				variants: [
+					{
+						weight: 400,
+						style: "normal",
+						src: ["./src/assets/PPMondwest-Regular.woff2"],
+					},
+				],
+			},
+		],
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
