859c8ecc96441a6928a8734448a80678e9be9512
John Meguerian <johnmeguerian@gmail.com>
2025-09-24 20:02:15 -0400

    Revert "Add nuxthub"
    Can't use nuxthub AND vercel simultaneously.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,40 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-				compatibilityDate: "2025-07-15",
-				devtools: { enabled: true },
-				modules: [
-				 "@nuxt/fonts",
-				 "@nuxt/scripts",
-				 "@nuxthub/core",
-				],
-				css: ["~/assets/css/globals.css"],
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/fonts", "@nuxt/scripts"],
+	css: ["~/assets/css/globals.css"],
 
-				features: {
-								noScripts: true,
-								inlineStyles: true,
-				},
+	features: {
+		noScripts: true,
+		inlineStyles: true,
+	},
 
-				scripts: {
-								registry: {
-												fathomAnalytics: {
-																site: "TVQPDWBA",
-												},
-								},
-				},
+	scripts: {
+		registry: {
+			fathomAnalytics: {
+				site: "TVQPDWBA",
+			},
+		},
+	},
 
-				app: {
-								head: {
-												title: "John Meguerian",
-												htmlAttrs: {
-																lang: "en",
-												},
-												meta: [
-																{
-																				name: "description",
-																				content: "Design Manager, Brooklyn, NY",
-																},
-												],
-								},
+	app: {
+		head: {
+			title: "John Meguerian",
+			htmlAttrs: {
+				lang: "en",
+			},
+			meta: [
+				{
+					name: "description",
+					content: "Design Manager, Brooklyn, NY",
 				},
+			],
+		},
+	},
 
-				vite: {
-								plugins: [tailwindcss()],
-				},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
