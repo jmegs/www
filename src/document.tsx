4fdd4f60eed50fe0e3ba3c44c9caf71d3c4746bb
John Meguerian <johnmeguerian@gmail.com>
2025-12-03 13:07:59 -0500

    add viewport tag

--- src/document.tsx
+++ src/document.tsx
@@ -4,6 +4,7 @@ export const StaticDocument = ({ children }) => (
 	<html lang="en">
 		<head>
 			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 			<link
 				rel="preconnect"
